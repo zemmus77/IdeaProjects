06fd8c708960ec33ec83294a9b57ebf8ffeb13f2
Serdar Muhammedov <muhammedoff@gmail.com>
2020-02-23 17:32:58 -0500

    Methods - Character Actions

---
+++ src/Day2/VariableIntro.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class VariableIntro {
+    public static void main(String[] args) {
+        int offerCnt = 3 ;
+
+        int corollaMileage = 5000 ;
+
+        System.out.println( offerCnt);
+
+        System.out.println( corollaMileage);
+
+    }
+
+
+    }
